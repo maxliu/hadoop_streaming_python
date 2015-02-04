086a9bfd3c609146b0fc76caad4ab8be697618fa
Xinyu Max Liu <xinyulrsm@gmail.com>
2015-02-04 08:49:11 -0800

    fixed tab and space mixed issue for pi files

--- pi/mapper.py
+++ pi/mapper.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 """
+This is the mapper function for pi estiation.
+
+Xinyu Max liu <xinyulrsm@gmail.com>
+12/2014
 """
 
 import sys
@@ -7,15 +11,23 @@ from random import random
 
 
 def get_ran(totalPoints=100):
+    """
+    generate a pairs of random number
+    """
     for i in xrange(totalPoints):
         yield [random(), random()]
 
 
 def mapper(totalPoints=100):
     coor = get_ran(totalPoints)
-    countInCircle = sum([1 for x in coor if (x[0]*x[0]+x[1]*x[1]<=1) ])
-    print "%d\t%f" % (1,4*float(countInCircle )/float(totalPoints))
+    countInCircle = sum([1 for x in coor if (x[0] * x[0] + x[1] * x[1] <= 1)])
+    print "%d\t%f" % (1, 4 * float(countInCircle) / float(totalPoints))
 
 if __name__ == "__main__":
-	totalPoints = int(sys.argv[1])
-	mapper(totalPoints = totalPoints)
+    """
+    read total points from command line
+    for example:
+    ./mapper.sh  100
+    """
+    totalPoints = int(sys.argv[1])
+    mapper(totalPoints=totalPoints)
--- pi/reducer.py
+++ pi/reducer.py
@@ -1,24 +1,35 @@
 #! /usr/bin/env python
 """
+This is the reducer function for pi estimation.
 
+Xinyu Max liu <xinyulrsm@gmail.com>
+12/2014
 """
 
 import sys
 
+
 def get_mapper_values():
-	for line in sys.stdin:
-		vs = line.split('\t',1)[1]
-		yield float(vs)
+    """
+    read resutls from mapper only return the value
+    """
+    for line in sys.stdin:
+        vs = line.split('\t', 1)[1]
+        yield float(vs)
+
 
 def reducer():
-	mv = get_mapper_values()
-	sumVal = 0
-	totalCount = 0
-	for v in mv:
-		sumVal += v
-		totalCount +=1
-    
-	
-	print sumVal /float(totalCount)
+    """
+    sum all the value and calculate the probality : pi
+    """
+    mv = get_mapper_values()
+    sumVal = 0
+    totalCount = 0
+    for v in mv:
+        sumVal += v
+        totalCount += 1
+    print sumVal / float(totalCount)
+
+
 if __name__ == "__main__":
-	reducer()
+    reducer()
